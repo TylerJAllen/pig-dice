3800e8b44cccc087b679cf929660c72e702bda81
ProgramJustin <programjustin@gmail.com>
2017-05-09 21:02:58 -0700

    Add files via upload

--- pig-dice/js/scripts.js
+++ pig-dice/js/scripts.js
@@ -1,1 +1,26 @@
-alert("flex the sauce");
+function Player(name, age, score) {
+  this.name = name;
+  this.age = age;
+  this.score = score;
+}
+
+// Player.prototype.score = function() {
+//
+// }
+
+function getRandomInt(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min) + 1);
+
+}
+alert(getRandomInt(1, 7));
+
+$(document).ready(function() {
+
+this.playerOne = new Player(name, age, score);
+this.playerTwo = new Player(name, age, score);
+
+
+
+});
